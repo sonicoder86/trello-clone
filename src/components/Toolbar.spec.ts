7eca7cfd756a98d3621d8f0589e248193cd255ac
Gabor Soos <soos.gabor86@gmail.com>
2022-06-19 15:49:12 +0200

    fix test

--- src/components/Toolbar.spec.ts
+++ src/components/Toolbar.spec.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
-import { Toolbar } from './';
+import Toolbar from './Toolbar.vue';
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
